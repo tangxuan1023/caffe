830483a9c84cdf9fec7cb3e1eab4e237a3c7fca8
tangxuan <Tx_huntaway1023@163.com>
2018-03-28 17:52:30 +0800

    read_code on wondows

---
+++ build/caffe_config.h
@@ -0,0 +1,12 @@
+/* Sources directory */
+#define SOURCE_FOLDER "D:/Documents/Study/Git/caffe"
+
+/* Binaries directory */
+#define BINARY_FOLDER "D:/Documents/Study/Git/caffe/build"
+
+/* This is an absolute path so that we can run test from any build
+ * directory */
+#define ABS_TEST_DATA_DIR "D:/Documents/Study/Git/caffe/src/caffe/test/test_data/"
+
+/* Test device */
+#define CUDA_TEST_DEVICE 
